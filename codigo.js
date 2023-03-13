154a393e6ab413c26e5aa108213b485da50f4d35
soranyir <soranyiibata@gmail.com>
2023-03-13 11:32:51 -0600

    modificaciones

---
+++ codigo.js
@@ -0,0 +1,11 @@
+document.getElementById("Sí").addEventListener("click", function() {
+    alert("sabia que dirias que sí Uwuuuu, saludame me amorr");
+
+});
+
+document.getElementById("no").addEventListener("mouseover", function(){
+    this.style.left = Math.random() *
+window.innerWidth + "px";
+    this.style.top = Math.random() *
+window.innerHeight + "px";
+});
